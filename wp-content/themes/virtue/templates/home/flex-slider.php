69bb1c2da9e306058bf880c15bfd8aa962904186
buntyoberoi <buntyoberoi451@gmail.com>
2016-09-30 15:37:42 +0530

    changes for dayanand school and home page theaming

--- wp-content/themes/virtue/templates/home/flex-slider.php
+++ wp-content/themes/virtue/templates/home/flex-slider.php
@@ -46,7 +46,10 @@
             <?php foreach ($slides as $slide) : 
                     if(!empty($slide['target']) && $slide['target'] == 1) {$target = '_blank';} else {$target = '_self';}
                     $image = aq_resize($slide['url'], $slidewidth, $slideheight, true);
-                    if(empty($image)) {$image = $slide['url'];} ?>
+                    if(empty($image)) {$image = $slide['http://dpsshimla.org/MyImages/LOGO.png'];} ?>
+
+
+                    
                       <li> 
                         <?php if($slide['link'] != '') echo '<a href="'.esc_url($slide['link']).'" target="'.esc_attr($target).'">'; ?>
                           <img src="<?php echo esc_url($image); ?>" width="<?php echo esc_attr($slidewidth);?>" height="<?php echo esc_attr($slideheight);?>" alt="<?php echo esc_attr($slide['title']); ?>" />
